d7769d731f786c27d730c47ab64a7aa44fa1f0d0
Kevin Ybarra <kevin.ybarra@corteva.com>
2023-06-14 11:19:19 -0300

    moved Head tag from index.ts to MainLayout.ts

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -1,21 +1,26 @@
+import Head from 'next/head'
 import React from 'react'
 import NavBar from '../components/NavBar'
 
 export default function MainLayout({ children }: any) {
     return (
         <>
+            <Head>
+                <title>Central de Canchas</title>
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
             <NavBar />
             <main className='mx-auto min-h-screen'>{children}</main>
             <footer className='w-full bg-black'>
-                <div className='flex justify-between items-center h-20 mx-4 text-slate-400'>
-                    <div>
-                        <span style={{color: 'rgb(148 163 184 / 1)'}}>@2022 Central de Canchas </span>
-                        <span style={{color: 'rgb(252 231 243 / 1)'}}>by KY</span>
+                <div className='flex items-center h-20 mx-4 text-slate-400'>
+                    <div className='w-1/3'>
+                        <span style={{ color: 'rgb(148 163 184 / 1)' }}>@2022 Central de Canchas </span>
+                        <span style={{ color: 'rgb(252 231 243 / 1)' }}>by KY</span>
                     </div>
-                    <div>
+                    <div className='w-1/3 flex justify-center'>
                         <img width={65} src="/KY-logo.png" className='rounded-full' alt="" />
                     </div>
-                    <div className='text-white'>SOCIAL MEDIA</div>
+                    <div className='text-white w-1/3 flex justify-end'>SOCIAL MEDIA</div>
                 </div>
             </footer>
         </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,13 +3,9 @@ import Head from 'next/head'
 import { useContext } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 
-const Home: NextPage = () => {
+export default function Home() {
 	return (
-		<div className='mx-auto h-5/6'>
-			<Head>
-				<title>Create Next App</title>
-				<link rel="icon" href="/favicon.ico" />
-			</Head>
+		<div className='mx-auto h-screen'>
 			<div className='flex flex-col justify-center items-center h-full'>
 				<img src='/court-finder-icon-big.jpg' alt="Logo" className='py-4 rounded-full' />
 				<div className='text-center font-mono tracking-widest font-semibold text-2xl'>Bienvenido a Central de Canchas</div>
@@ -17,5 +13,3 @@ const Home: NextPage = () => {
 		</div>
 	)
 }
-
-export default Home
