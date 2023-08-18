2c817ba6cfb3dbd364aae4f0834059556cf012c4
Pro <pro@MacBook-Pro-de-Pro.local>
2023-08-18 17:10:17 -0300

    fix(navbar): Changed the login button's icon

--- components/navBar/CredentialButtons.tsx
+++ components/navBar/CredentialButtons.tsx
@@ -1,23 +1,55 @@
-import React from 'react'
-import NavBarButton from './NavBarButton'
+import React from "react";
+import NavBarButton from "./NavBarButton";
 
 const loginIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 lg:w-6 lg:h-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M16.5 10.5V6.75a4.5 4.5 0 10-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 002.25-2.25v-6.75a2.25 2.25 0 00-2.25-2.25H6.75a2.25 2.25 0 00-2.25 2.25v6.75a2.25 2.25 0 002.25 2.25z" />
-    </svg>
-)
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    stroke="currentColor"
+    className="w-5 h-5 lg:w-6 lg:h-6"
+  >
+    <path
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      d="M15.75 9V5.25A2.25 2.25 0 0013.5 3h-6a2.25 2.25 0 00-2.25 2.25v13.5A2.25 2.25 0 007.5 21h6a2.25 2.25 0 002.25-2.25V15m3 0l3-3m0 0l-3-3m3 3H9"
+    />
+  </svg>
+);
 
 const registerIcon = (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 lg:w-6 lg:h-6 stroke-slate-200">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z" />
-    </svg>
-)
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="w-5 h-5 lg:w-6 lg:h-6 stroke-slate-200"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M19 7.5v3m0 0v3m0-3h3m-3 0h-3m-2.25-4.125a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zM4 19.235v-.11a6.375 6.375 0 0112.75 0v.109A12.318 12.318 0 0110.374 21c-2.331 0-4.512-.645-6.374-1.766z"
+    />
+  </svg>
+);
 
 export default function CredentialButtons() {
-    return (
-        <>
-            <NavBarButton dark={false} to='/ingresar' name='ingresar' icon={loginIcon} />
-            <NavBarButton dark={true} to='/registro' name='registrarse' icon={registerIcon} />
-        </>
-    )
+  return (
+    <>
+      <NavBarButton
+        dark={false}
+        to="/ingresar"
+        name="ingresar"
+        icon={loginIcon}
+      />
+      <NavBarButton
+        dark={true}
+        to="/registro"
+        name="registrarse"
+        icon={registerIcon}
+      />
+    </>
+  );
 }
