0cb28a9675c5afeee2b1ddd4f380c89a56b56384
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-06 02:47:00 -0300

    refactor(navBar): Replaced the 'tutorial' section from the navbar for the 'canchas' section

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -42,8 +42,8 @@ export default function NavBar() {
                             </Link>
                         </li>
                         <li onClick={toggleNavbar}>
-                            <Link href='/tutorial'>
-                                <div className={'cursor-pointer w-1/2 lg:w-auto m-auto my-1 lg:my-0 px-5 py-1 text-slate-800 hover:text-slate-400 transition-all ' + (currentPage.pathname === '/proximamente' ? "border rounded-2xl bg-slate-300 hover:text-slate-600" : "")}>TUTORIAL</div>
+                            <Link href='/canchas'>
+                                <div className={'cursor-pointer w-1/2 lg:w-auto m-auto my-1 lg:my-0 px-5 py-1 text-slate-800 hover:text-slate-400 transition-all ' + (currentPage.pathname === '/canchas' ? "border rounded-2xl bg-slate-300 hover:text-slate-600" : "")}>CANCHAS</div>
                             </Link>
                         </li>
                         <li onClick={toggleNavbar}>
