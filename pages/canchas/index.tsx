8f97a03497d25a1468ba271e573ce304a9fff173
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-07 16:47:09 -0300

    feat(courts): Added the delete action on the courts page

--- pages/canchas/index.tsx
+++ pages/canchas/index.tsx
@@ -1,19 +1,27 @@
-import React, { ReactElement, useEffect, useState } from "react";
+import React, { ChangeEvent, ReactElement, useEffect, useState } from "react";
 import markerService from "../../firebase/marker.service";
 import { MarkerDataInterface } from "../../types/Map.interface";
 import {
     CourtsTableData,
     PaginatedCourtsData,
 } from "../../types/CourtsData.interface";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { DocumentData, QueryDocumentSnapshot } from "firebase/firestore";
 import { Headers } from "../../constans/tableHeaders.constant";
+import ModalWithOptions from "../../components/ModalWithOptions";
 
 export default function canchas() {
     const [courtsData, setCourtsData] = useState<CourtsTableData[]>([]);
     const [headers, setHeaders] = useState<string[]>([]);
-    const [lastCourtCalled, setLastCourtCalled] = useState<QueryDocumentSnapshot<DocumentData> | null>(null);
+    const [lastCourtCalled, setLastCourtCalled] =
+        useState<QueryDocumentSnapshot<DocumentData> | null>(null);
+
     const [loadingData, setLoadingData] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [aboutToDeleteCourt, setAboutToDeleteCourt] = useState<{index: number, courtName: string}>({
+        index: 0,
+        courtName: ''
+    });
 
     useEffect(() => {
         const fetchCourtsData = async () => {
@@ -30,11 +38,30 @@ export default function canchas() {
 
     async function loadMoreCourts() {
         setLoadingData(true);
-        const newCourtsData = (await markerService.getPaginatedCourtsData(lastCourtCalled));
+        const newCourtsData = await markerService.getPaginatedCourtsData(
+            lastCourtCalled
+        );
         const newCourts = [...courtsData, ...newCourtsData.courtsData];
         setCourtsData(newCourts);
         setLastCourtCalled(newCourtsData.lastCourtCalled);
         setLoadingData(false);
+    }
+
+    async function deleteCourt() {
+        try {
+            await markerService.deleteCourt(aboutToDeleteCourt.courtName);
+            setAboutToDeleteCourt({
+                index: 0,
+                courtName: ''
+            });
+            const newCourts = [...courtsData]
+            newCourts.splice(aboutToDeleteCourt.index, 1);
+            setCourtsData(newCourts);
+            setIsModalOpen(false);
+
+        } catch(e: any) {
+            console.log(e);
+        }
     }
 
     const loadingIcon = (
@@ -64,6 +91,23 @@ export default function canchas() {
         </svg>
     );
 
+    const deleteIcon = (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="w-6 h-6 cursor-pointer"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+            />
+        </svg>
+    );
+
     return (
         <div className="w-full min-h-screen overflow-x-auto">
             {courtsData.length ? null : (
@@ -71,36 +115,67 @@ export default function canchas() {
                     {loadingIcon}
                 </div>
             )}
-            <div className={`main-grid min-h-full w-[250%] lg:w-full grid grid-rows-[repeat(${courtsData.length + 2},_minmax(0,_1fr))]`}>
-                <div className="headers w-full grid grid-cols-[repeat(9,_minmax(0,_1fr))] text-[12px] lg:text-base">
+            <div className={`main-grid min-h-full w-[250%] lg:w-full grid grid-rows-[repeat(${courtsData.length + 2 },_minmax(0,_1fr))]`}>
+                <div className="headers w-full grid grid-cols-[repeat(10,_minmax(0,_1fr))] text-[12px] lg:text-base">
                     {headers.map((header: string) => {
                         return (
-                            <div key={uuidv4()} className="single-header flex justify-center items-center py-1 text-center bg-gray-200 border border-gray-300 font-basketball tracking-widest">
+                            <div
+                                key={uuidv4()}
+                                className="single-header flex justify-center items-center py-1 text-center bg-gray-200 border border-gray-300 font-basketball tracking-widest"
+                            >
                                 {Headers[header as keyof typeof Headers]}
                             </div>
                         );
                     })}
+                    <div
+                        key={uuidv4()}
+                        className="single-header flex justify-center items-center py-1 text-center bg-gray-200 border border-gray-300 font-basketball tracking-widest"
+                    >
+                        acciones
+                    </div>
                 </div>
-                {courtsData.map((singleCourtData) => {
+                {courtsData.map((singleCourtData, index) => {
                     const dataRow = [];
                     for (let key in singleCourtData) {
                         dataRow.push(
-                            <div key={uuidv4()} className="single-cell flex justify-center items-center text-center py-4 px-2 border">
+                            <div
+                                key={uuidv4()}
+                                className="single-cell flex justify-center items-center text-center py-4 px-2 border"
+                            >
                                 {singleCourtData[key as keyof CourtsTableData]}
                             </div>
                         );
                     }
+                    dataRow.push(
+                        <div
+                            id={singleCourtData.courtName}
+                            key={index}
+                            className="single-cell flex justify-center items-center text-center py-4 px-2 border"
+                            onClick={(e) => {
+                                setAboutToDeleteCourt({
+                                    index: index,
+                                    courtName: e.currentTarget.id
+                                })
+                                setIsModalOpen(true);
+                            }}
+                        >
+                            {deleteIcon}
+                        </div>
+                    );
                     return (
-                        <div key={uuidv4()} className="row w-full grid grid-cols-[repeat(9,_minmax(0,_1fr))] text-[12px] lg:text-base">
+                        <div
+                            key={uuidv4()}
+                            className="row w-full grid grid-cols-[repeat(10,_minmax(0,_1fr))] text-[12px] lg:text-base"
+                        >
                             {dataRow}
                         </div>
                     );
                 })}
-                {!courtsData.length ? null : (
-                    loadingData ? 
+                {!courtsData.length ? null : loadingData ? (
                     <div className="flex justify-center items-center my-4">
                         {loadingIcon}
-                    </div> : 
+                    </div>
+                ) : (
                     <div className="footer-container flex justify-center items-center my-4 bg-gray rounded-b-[50px]">
                         <button
                             disabled={!lastCourtCalled}
@@ -112,6 +187,17 @@ export default function canchas() {
                     </div>
                 )}
             </div>
+            {isModalOpen && (
+                <ModalWithOptions
+                    message={'Seguro quiere eliminar la cancha permanentemente?'}
+                    closeModal={() => {
+                        setIsModalOpen(false);
+                    }}
+                    confirmAction={() => {
+                        deleteCourt();
+                    }}
+                ></ModalWithOptions>
+            )}
         </div>
     );
 }
