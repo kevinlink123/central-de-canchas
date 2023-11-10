f903c42f2210480fbc80678b05fbae1ae6084f5e
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-10 20:38:00 -0300

    refactor(mapInterface): Removed unused newMarkerDataInterface

--- types/Map.interface.ts
+++ types/Map.interface.ts
@@ -1,11 +1,9 @@
 import { LatLngTuple } from "leaflet";
 
-export interface MarkerDataInterface extends NewMarkerDataInterface {
-    coordinates: LatLngTuple;
-}
-
-export interface NewMarkerDataInterface extends NewMarkerFormDataInterface {
+export interface MarkerDataInterface extends NewMarkerFormDataInterface {
+    id: string;
     uid: string;
+    coordinates: LatLngTuple;
 }
 
 export interface NewMarkerFormDataInterface {
