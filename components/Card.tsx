fc0f98c83bf5d6f2cf8009cd0e30f4c1cc746443
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-10 21:32:13 -0300

    refactor(card): Added minor changes to Card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,14 +1,15 @@
+import { LatLngTuple } from "leaflet";
 import React, { ReactElement } from "react";
 
 interface CardProps {
     logo: ReactElement;
     infoName: string;
-    data: string;
+    data: string | LatLngTuple;
 }
 
 export default function Card(props: CardProps) {
     return (
-        <div className="info-card grid grid-rows-3 text-center items-center bg-gray-400 mx-6">
+        <div className="info-card grid grid-rows-3 text-center items-center bg-gray-400 mx-6 py-8 px-2 rounded-lg">
             <div>{props.logo}</div>
             <div className="text-center">{props.infoName}</div>
             {props.data}
