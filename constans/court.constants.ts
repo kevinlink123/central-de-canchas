953381dea67025328ca88d25e16a533a894d9780
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-10 21:31:01 -0300

    feat(constants): Added court page constants

---
+++ constans/court.constants.ts
@@ -0,0 +1,9 @@
+export enum Headers {
+    'surfaceType' = 'Superficie del piso',
+    'numberOfHoops' = 'Aros Disponibles',
+    'numberOfCourts' = 'Canchas Disponibles',
+    'rimHeight' = 'Altura de los Aros',
+    'rimCondition' = 'Condicion de los Aros',
+}
+
+export const courtDataTitles = ['surfaceType', 'numberOfHoops', 'numberOfCourts', 'rimHeight', 'rimCondition'];
