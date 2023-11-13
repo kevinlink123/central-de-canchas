3e43cd04a8693785e120ae6e95931abad37b79be
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-13 20:17:50 -0300

    fix(card): Minor fixes to the styles

--- components/Card.tsx
+++ components/Card.tsx
@@ -13,7 +13,7 @@ export default function Card(props: CardProps) {
         <div className="info-card flex flex-col justify-around items-center text-center w-full mx-6 px-2 py-4 rounded-lg bg-gray-100 shadow-gray-200 shadow-lg">
             <div className="w-full flex justify-center">{props.logo}</div>
             <div className="text-center">{props.infoName}</div>
-            <div className="font-bold py-3">{data}</div>
+            <div className="font-bold">{data}</div>
         </div>
     );
 }
