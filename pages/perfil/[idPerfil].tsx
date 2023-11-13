af721841287d7853ddfff49bd277aa3afb52ce58
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-13 02:11:50 -0300

    refactor(profile): Added a conditional statement that ensures that when the component fetch profile info only when the url path is available

--- pages/perfil/[idPerfil].tsx
+++ pages/perfil/[idPerfil].tsx
@@ -2,22 +2,25 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import authService from '../../firebase/auth.service';
 import { DocumentData } from '@firebase/firestore';
+import { UserData } from '../../types/UserData.interface';
 
 export default function () {
-    const [userData, setUserData] = useState<DocumentData | undefined>();
+    const [userData, setUserData] = useState<UserData>();
     const [loading, setLoading] = useState(true);
 
     const router = useRouter();
     const { idPerfil } = router.query;
 
     useEffect(() => {
+        if(!idPerfil) { return };
+
         const fetchUserData = async () => {
             const userData = await authService.getProfile(idPerfil as string);
             setUserData(userData);
             setLoading(false);
         }
         fetchUserData();
-    }, [])
+    }, [idPerfil])
 
     return (
         <div className='flex justify-center items-center'>
