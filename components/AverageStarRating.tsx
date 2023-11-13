4b1b99ac2c9f8fa8ffa452d0c623adf4650f1352
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-13 02:04:24 -0300

    feat(averageStarRating): Added average star rating component to show the average stars that a court has.

---
+++ components/AverageStarRating.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+import React, { useState } from "react";
+
+interface AverageStarRatingProps {
+    rating: number;
+}
+
+export default function AverageStarRating(props: AverageStarRatingProps) {
+    return (
+        <div className="star-rating">
+            <div className="my-4">Calificación promedio</div>
+            {[...Array(5)].map((star, index) => {
+                index += 1;
+                return (
+                    <span className="mx-0.5">
+                        <Image
+                            src={"/icons/basketball.svg"}
+                            width={18}
+                            height={18}
+                            className={index <= props.rating ? "" : "filter brightness-0"}
+                        />
+                    </span>
+                );
+            })}
+        </div>
+    );
+}
