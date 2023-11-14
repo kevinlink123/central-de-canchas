f14cc18ba445dc5063d3372f78ca4ac4c933eadb
Pro <pro@MacBook-Pro-de-Pro.local>
2023-11-14 17:35:46 -0300

    fix(card): Added mobile-only styles

--- components/Card.tsx
+++ components/Card.tsx
@@ -10,7 +10,7 @@ interface CardProps {
 export default function Card(props: CardProps) {
     const { data } = props;
     return (
-        <div className="info-card flex flex-col justify-around items-center text-center w-full mx-6 px-2 py-4 rounded-lg bg-gray-100 shadow-gray-200 shadow-lg">
+        <div className="info-card flex flex-col justify-around items-center text-center w-full my-5 lg:my-0 mx-2 lg:mx-6 px-2 py-4 rounded-lg bg-gray-100 shadow-gray-200 shadow-lg">
             <div className="w-full flex justify-center">{props.logo}</div>
             <div className="text-center">{props.infoName}</div>
             <div className="font-bold">{data}</div>
