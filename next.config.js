2fcfc2e66e322c9d4f86830cc82ec767df9f8cd3
Pro <pro@MacBook-Pro-de-Pro.local>
2023-12-01 13:04:53 -0300

    feat(app): Added support for PWA. Added manifest.json file, _document.tsx linking all necessary files and added config for PWA inside next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,24 @@
+const withPWA = require("next-pwa")({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+    disable: process.env.NODE_ENV === "development",
+});
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const path = require("path");
+
+const nextConfig = {
     reactStrictMode: true,
     async redirects() {
         return [
-			{
-				source: '/tutorial',
-				destination: '/proximamente',
-				permanent: false
-			}
+            {
+                source: "/tutorial",
+                destination: "/proximamente",
+                permanent: false,
+            },
         ];
     },
 };
+
+module.exports = nextConfig;
