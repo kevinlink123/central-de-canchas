d1d291ce985ae464431ee5876e935ce49101f1ea
kevinlink123 <kevin_link_23@hotmail.com>
2025-07-11 11:24:41 -0300

    refactor(homePage): Deleted unnecessary div tag and corrected styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,9 @@ import { AuthContext } from '../contexts/AuthContext'
 
 export default function Home() {
 	return (
-		<div className='mx-auto h-screen'>
-			<div className='flex flex-col justify-center items-center h-full'>
-				<img src='/court-finder-icon-big.jpg' alt="Logo" className='py-4 rounded-full' />
-				<div className='text-center font-mono tracking-widest font-semibold text-2xl'>Bienvenido a Central de Canchas</div>
-			</div>
+		<div className='flex flex-col justify-center items-center h-screen'>
+			<img src='/court-finder-icon-big.jpg' alt="Logo" className='py-4 rounded-full' />
+			<div className='text-center font-mono tracking-widest font-semibold text-2xl'>Bienvenido a Central de Canchas</div>
 		</div>
 	)
 }
