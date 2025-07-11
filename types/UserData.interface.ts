374b4c1c2a8c02b349bc0f9cbc22b1d03225ed21
kevinlink123 <kevin_link_23@hotmail.com>
2025-07-11 11:27:21 -0300

    fix(userDataInterface): Corrected the types for the user data interface

--- types/UserData.interface.ts
+++ types/UserData.interface.ts
@@ -1,7 +1,8 @@
 export interface UserData {
     id: string;
     email: string;
-    registeredCourts: string;
-    roles: string;
+    registeredCourts: string[];
+    favoriteCourts: [],
     username: string;
+    roles: string[];
 }
